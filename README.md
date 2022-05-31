e2190c7eb8e81812ab417e869ea2de8ca8b7c450
Samir Gayatri <samirgupta19670@gmail.com>
2022-05-31 19:14:53 +0530

    initial-commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Amazon Clone with ReactJS
+In this Amazon Clone application you can register on the site, log in with your account, shop, pay for the products and see a list of the products you bought.
+
+## Amazon Clone Demo Link
+<a href="https://clone-ff34b.firebaseapp.com/">click me</a>   with typing credit card number as: 4242 4242 4242 4242 cvv:42424 valid date:0424
+
+
+## Topics
++ Build checkout and payment page with stripe functionality
++ Checkout screen with the React context API
++ Accounts and Login Page with Firebase Authentication
++ Deploy cloud function to process payment
++ Firebase for real time database
++ Deploy the App LIVE on Firebase
++ Store orders for logged in user
+
+## Author
+Mert Çankaya
+
---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React from "react"
+import './App.css';
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Checkout from "./Components/Checkout";
+import Login from "./Components/Login";
+import { useEffect } from "react";
+import { auth } from "./firebase";
+import { useStateValue } from "./StateProvider";
+import Payment from "./Components/Payment";
+import { loadStripe } from "@stripe/stripe-js"
+import { Elements } from "@stripe/react-stripe-js"
+import Orders from "./Components/Orders";
+import Footer from "./Components/Footer";
+
+const promise = loadStripe("pk_test_51JC4lzIHdhuPJfTIBk3wZPJGClqNjOSX9ERCv6wXm1vsYq5NblDo2OGdsZo5OZFfEXdXBhCdZUqE4QSblUWSJRAb00hXli22Py")
+
+function App() {
+  const [{data}, dispatch] = useStateValue();
+  console.log("App Data:", data);
+
+  useEffect(() => {
+    auth.onAuthStateChanged(authUser => {
+
+      if (authUser) {
+
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        })
+      } else {
+        dispatch({
+          type: "SET_USER",
+          user: null
+        })
+      }
+
+
+    })
+  }, [])
+
+  return (
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/orders">
+            <Header />
+            <Orders />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+          <Route path="/payment">
+            <Header />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+            <Footer />
+          </Route>
+        </Switch>
+
+      </div>
+    </Router>
+  );
+}
+
+export default App;
